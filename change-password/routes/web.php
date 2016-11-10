028f1f96f05826f722437cd32deb3b4356a36d0d
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-11-10 14:57:01 +0700

    Change password

--- change-password/routes/web.php
+++ change-password/routes/web.php
@@ -9,8 +9,17 @@
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('password', 'PasswordController@change')->name('password.change');
+    Route::put('password', 'PasswordController@update')->name('password.update');
 });
