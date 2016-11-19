b14a9fce2430feede4bb6ae736b6e9d34cd8944d
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-11-19 12:05:12 +0700

    Unused files

--- sms-gateway-me/sms-gateway-me/config/smsgateway.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'email'    => env('SMS_EMAIL', ''),    // insert your email here
-    'password' => env('SMS_PASSWORD', ''), // insert your password here
-    'device'   => env('SMS_DEVICE', ''),   // insert your device ID here
-];
